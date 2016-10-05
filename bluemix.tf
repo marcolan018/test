5ff42d8486066e999128bd3aca3d9b626e770460
LanLing <blueling@gmail.com>
2016-10-05 15:25:49 -0500

    Update bluemix.tf

--- bluemix.tf
+++ bluemix.tf
@@ -1,18 +1,3 @@
-variable "region" {
-  default = "ng"
-}
-variable "image" {
-  type = "map"
-  default = {
-    ng = "registry.ng.bluemix.net/ibmnode:latest"
-    eu-gb = "registry.ng.bluemix.net/ibmnode:latest"
-  }
-}
-
-variable "name" {
-  default = "foo"
-}
-
 # Create a container
 resource "docker_container" "foo" {
     image = "${lookup(var.image, var.region)}"
